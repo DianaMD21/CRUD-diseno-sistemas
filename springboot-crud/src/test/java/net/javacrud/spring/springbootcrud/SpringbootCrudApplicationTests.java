f70016aaa2e278104065c25cbb259caa3261dcdb
Diana <diana2monegro1@gmail.com>
2021-12-04 17:04:25 -0400

    Adding initial files of project

---
+++ springboot-crud/src/test/java/net/javacrud/spring/springbootcrud/SpringbootCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javacrud.spring.springbootcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
